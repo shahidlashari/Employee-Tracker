6294cbdc1aed1c0e8268cf354eae8efbc02d1221
shahid lashari <shahid_lashari001@yahoo.com>
2020-04-23 23:23:50 -0700

    created start function to render choices with iquirer and linked question file with index file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //Packages 
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-// const questions = require("./questions");
-// const consoleTable = require('console.table');
+const questions = require("./questions");
+const consoleTable = require('console.table');
 
 //setting up the connection with the DB
 const connection = mysql.createConnection({
@@ -18,6 +18,42 @@ const connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log ("I am connected");
 });
+//initial function that will input the  user what they would like to do 
+start()
+async function start() {
+
+    const results = await inquirer.prompt(questions.actionList);
+    switch (results.actionList) {
+        case 'Add new employee': 
+            addEmployee();
+            break;
+        case 'View all employees': 
+            viewAll();
+            break;
+        case 'View employees by department':
+            viewBydepartment();
+            break;
+        case 'Update employee role': 
+            updateRole();
+            break;
+        case 'View all roles':
+            viewAllRoles(); 
+            break;
+        case "Add role":
+            addRole(); 
+            break;
+        case 'View all departments':
+            viewAlldepartments(); 
+            break;
+        case 'Add department':
+            addDpt(); 
+            break;
+
+        default:
+            connection.end();
+            break;
+
+    }
+}
 
